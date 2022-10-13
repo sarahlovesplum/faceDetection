47e5a5667f8c134de4807ddc2f74f9c7475af289
leeseyoung <leeseyoung@team-lab.com>
2022-10-13 21:02:06 +0900

    age&gender 追加

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const video = document.getElementById('video')
+const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./models')
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"), //작은얼굴 잡는거
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"), //눈코입
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"), //얼굴 따라다니는 박스
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"), //슬픔 웃음 눈물 같은거
+  faceapi.nets.ageGenderNet.loadFromUri("./models"),
+]).then(startVideo);
 
 function startVideo() {
   navigator.mediaDevices
@@ -14,21 +15,32 @@ function startVideo() {
       video.srcObject = stream;
     })
     .catch(function (err) {
-      console.log(err);
+      console.error(err);
     });
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()) //카메라 안에 얼굴 모두 인식
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    resizedDetections.forEach((detection) => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: Math.round(detection.age) + " year old " + detection.gender,
+      });
+      drawBox.draw(canvas);
+    });
+  }, 100);
+});
